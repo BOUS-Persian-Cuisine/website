21092bf3a71938683cfbfa17214f75a3e6030508
Amir Saboury <amir.saboury@gmail.com>
2026-05-08 16:45:57 -0400

    Updates

--- _next/static/chunks/ef87a6fac07ac4a7.js
+++ _next/static/chunks/ef87a6fac07ac4a7.js
@@ -1,7 +1,7 @@
 __turbopack_load_page_chunks__("/about", [
   "static/chunks/98817df83a267437.js",
-  "static/chunks/f388fbcd03ab66da.js",
+  "static/chunks/ce1bc55e60abe1bb.js",
   "static/chunks/e9f79bce612458fa.js",
   "static/chunks/07d7c4349c511065.js",
-  "static/chunks/turbopack-c80d9faffb0e44ff.js"
+  "static/chunks/turbopack-fb6f7fb864b48c42.js"
 ])
--- _next/static/tjRWB1W9muMjF6OdwEryv/_buildManifest.js
+++ _next/static/tjRWB1W9muMjF6OdwEryv/_buildManifest.js
@@ -1,15 +1,15 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/7a1eefd8b032affa.js"
+    "static/chunks/c0bb8479db269fd6.js"
   ],
   "/_error": [
     "static/chunks/e55b0d6b5dc1d9cf.js"
   ],
   "/about": [
-    "static/chunks/c2dba5ed4139638b.js"
+    "static/chunks/ef87a6fac07ac4a7.js"
   ],
   "/contact": [
-    "static/chunks/7d51113f4e5417ae.js"
+    "static/chunks/bc91cf29b974c26f.js"
   ],
   "__rewrites": {
     "afterFiles": [],
