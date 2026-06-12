595074f13a1737457b9bd42d338367aef86a83cd
Amir Saboury <amir.saboury@gmail.com>
2026-06-12 00:36:59 -0400

    feat: add google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,33 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
+const googleTagManagerId = "GTM-5RBBHRJQ";
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${googleTagManagerId}');
+            `,
+          }}
+        />
+      </Head>
       <body className="antialiased bg-background text-foreground">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
